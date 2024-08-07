38c62b6732217994d1dcd3752775996809fa79fe
Betselot Tesfa <99252072+betselotT@users.noreply.github.com>
2024-08-07 22:54:00 +0300

    Time: 69 ms (39.10%) | Memory: 16.6 MB (7.71%) - LeetSync

---
+++ 755-reach-a-number/reach-a-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        ans = 0 
+        step = 0
+        target = abs(target)
+        while ans < target or (ans -target) % 2 != 0:
+            step += 1
+            ans += step
+           
+        return step
