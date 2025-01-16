22489c3a2cdbf3caeeb28c4f2de4a86610468a0e
Betselot Tesfa <99252072+betselotT@users.noreply.github.com>
2025-01-16 16:53:11 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (6.84%) - LeetSync

---
+++ 61-rotate-list/rotate-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        length,tail=1,head
+        cur=head
+        while tail.next:
+            tail=tail.next
+            length+=1
+        k=k%length
+        if k==0:
+            return head
+        for i in range(length-k-1):
+            cur=cur.next
+        new=cur.next
+        cur.next=None
+        tail.next=head
+        return new
